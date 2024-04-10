d00b08dde8550fb9523419bf69600d4df6813efd
AgustinBadi <absalinas@uc.cl>
2024-04-10 16:25:01 -0400

    Fix conversion.sh && fix docs

--- convert.sh
+++ convert.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 
-# Step 1: Echo "Path of your file?"
-echo "Folder of your circom files?"
 
-# Step 2: Take the relative path of the file from stdin and stored it in a variable called PROOF_PATH.
-read CIRCOM_FILES_PATH
 
-# Step 3: cd conversion.
+CIRCOM_FILES_PATH=$1
+
 cd $CIRCOM_FILES_PATH || exit 1
-
-# Step 4: Inside conversion mkdir output.
-#mkdir -p conv_outputs
 
 #node ../conversion/index.js "$CIRCOM_FILES_PATH" > conv_outputs/output.txt || exit 1 
 node ../conversion/index.js "$CIRCOM_FILES_PATH" || exit 1 
 
-echo "Conversion done!"
+echo "Serialization done!"
 
 
 
