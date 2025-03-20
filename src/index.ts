4a9bc367ffc385fe9e00aa486bf91e779117576d
AgustinBadi <librenotgratis@tuta.io>
2025-03-20 14:57:38 -0300

    Add comments with steps to verify the three factorial proof on-chain

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+// Compile the circuit  
+
+// Generate the vk and pk  
+
+// Generate the proof  
+
+// Invoke provider.  
+
+// Generate a wallet  
+
+// Import the validator script.  
+
+/*  === Instantiate the contract ===  
+
+ Define a Tx that sends some Ada to the groth validator address.  
+
+ Calculate the script address.  
+ Define a datum that contains one of the factors and the product.  
+ Send some ADA to the address, generating a UTXO.  
+*/
+
+/*  === Verify the ZK proof ===  
+
+Define a Tx:  
+ Create a redeemer with the proof.  
+ Attach the contract.  
+ Attempt to spend the UTXO from the script address.  
+*/  
